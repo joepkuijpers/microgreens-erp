6599830011f124fca0512b2fa8e26c5600f7eeb5
Joep Kuijpers <a.likkle.more@gmail.com>
2026-06-30 17:20:40 +0200

    Dashboard sensorkaart gekoppeld aan laatste sensorlog

--- api/sensors.php
+++ api/sensors.php
@@ -1,10 +1,25 @@
 <?php
 header('Content-Type: application/json');
+include '../db_connect.php';
 
-echo json_encode([
-    "temperature" => "22.3",
-    "humidity" => "57",
-    "pressure" => "1013",
-    "light" => "8450"
-]);
+$stmt = $db->query("
+    SELECT timestamp, temperature, humidity, pressure, light
+    FROM sensor_log
+    ORDER BY id DESC
+    LIMIT 1
+");
+
+$data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if (!$data) {
+    $data = [
+        "timestamp" => null,
+        "temperature" => "--",
+        "humidity" => "--",
+        "pressure" => "--",
+        "light" => "--"
+    ];
+}
+
+echo json_encode($data);
 ?>
