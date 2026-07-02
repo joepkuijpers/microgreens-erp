2bdc33f3e96ba9b07e4898673190354b01060f08
Joep Kuijpers <a.likkle.more@gmail.com>
2026-07-02 10:45:58 +0200

    Use reusable page actions in page header

--- includes/page_header.php
+++ includes/page_header.php
@@ -6,21 +6,22 @@ $pageActions = $pageActions ?? [];
 ?>
 
 <div class="page-header">
-    <div>
-        <h1><?= htmlspecialchars($pageIcon . ' ' . $pageTitle) ?></h1>
+
+    <div class="page-header-left">
+
+        <h1>
+            <?= htmlspecialchars($pageIcon) ?>
+            <?= htmlspecialchars($pageTitle) ?>
+        </h1>
 
         <?php if (!empty($pageDescription)): ?>
             <p><?= htmlspecialchars($pageDescription) ?></p>
         <?php endif; ?>
+
     </div>
 
-    <?php if (!empty($pageActions)): ?>
-        <div class="page-actions">
-            <?php foreach ($pageActions as $action): ?>
-                <a class="button" href="<?= htmlspecialchars($action['href']) ?>">
-                    <?= htmlspecialchars($action['label']) ?>
-                </a>
-            <?php endforeach; ?>
-        </div>
-    <?php endif; ?>
+    <div class="page-header-right">
+        <?php include __DIR__ . '/page_actions.php'; ?>
+    </div>
+
 </div>
