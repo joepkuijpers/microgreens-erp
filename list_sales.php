b278d6d003544f74c2589089117714448c538c6a
Joep Kuijpers <a.likkle.more@gmail.com>
2026-07-10 00:58:01 +0200

    Add sticky header scroll container to sales table

--- list_sales.php
+++ list_sales.php
@@ -40,63 +40,65 @@ $total = $db->query("
         <h1>€ <?= number_format((float)$total['total'], 2, ',', '.') ?></h1>
     </div>
 
-    <div class="card">
-        <table>
-            <thead>
-                <tr>
-                    <th><?= htmlspecialchars(__('id')) ?></th>
-                    <th><?= htmlspecialchars(__('date')) ?></th>
-                    <th><?= htmlspecialchars(__('customer')) ?></th>
-                    <th><?= htmlspecialchars(__('product')) ?></th>
-                    <th><?= htmlspecialchars(__('sales_batch')) ?></th>
-                    <th><?= htmlspecialchars(__('sales_harvest')) ?></th>
-                    <th><?= htmlspecialchars(__('quantity')) ?></th>
-                    <th><?= htmlspecialchars(__('amount')) ?></th>
-                    <th><?= htmlspecialchars(__('status')) ?></th>
-                    <th><?= htmlspecialchars(__('details')) ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php if (count($sales) === 0): ?>
-                    <tr><td colspan="10"><?= htmlspecialchars(__('no_sales_registered')) ?></td></tr>
-                <?php endif; ?>
-
-                <?php foreach ($sales as $sale): ?>
+    <div class="card sales-table-card">
+        <div class="table-scroll">
+            <table>
+                <thead>
                     <tr>
-                        <td><?= htmlspecialchars((string)$sale['id']) ?></td>
-                        <td><?= htmlspecialchars((string)$sale['sale_date']) ?></td>
-                        <td><?= htmlspecialchars((string)($sale['customer_name'] ?? '-')) ?></td>
-                        <td><?= htmlspecialchars((string)($sale['product_name'] ?? '-')) ?></td>
-                        <td>
-                            <?php if (!empty($sale['batch_id'])): ?>
-                                <a href="batch_details.php?id=<?= urlencode((string)$sale['batch_id']) ?>">
-                                    <?= htmlspecialchars((string)$sale['batch_id']) ?>
-                                </a>
-                            <?php else: ?>
-                                -
-                            <?php endif; ?>
-                        </td>
-                        <td>
-                            <?php if (!empty($sale['harvest_id'])): ?>
-                                <a href="harvest_details.php?id=<?= urlencode((string)$sale['harvest_id']) ?>">
-                                    <?= htmlspecialchars((string)$sale['harvest_id']) ?>
-                                </a>
-                            <?php else: ?>
-                                -
-                            <?php endif; ?>
-                        </td>
-                        <td><?= number_format((float)$sale['quantity'], 2, ',', '.') ?></td>
-                        <td>€ <?= number_format((float)$sale['amount'], 2, ',', '.') ?></td>
-                        <td><?= htmlspecialchars((string)$sale['status']) ?></td>
-                        <td>
-                            <a class="btn" href="traceability_sale_detail.php?id=<?= urlencode((string)$sale['id']) ?>">
-                                🔎 <?= htmlspecialchars(__('details')) ?>
-                            </a>
-                        </td>
+                        <th><?= htmlspecialchars(__('id')) ?></th>
+                        <th><?= htmlspecialchars(__('date')) ?></th>
+                        <th><?= htmlspecialchars(__('customer')) ?></th>
+                        <th><?= htmlspecialchars(__('product')) ?></th>
+                        <th><?= htmlspecialchars(__('sales_batch')) ?></th>
+                        <th><?= htmlspecialchars(__('sales_harvest')) ?></th>
+                        <th><?= htmlspecialchars(__('quantity')) ?></th>
+                        <th><?= htmlspecialchars(__('amount')) ?></th>
+                        <th><?= htmlspecialchars(__('status')) ?></th>
+                        <th><?= htmlspecialchars(__('details')) ?></th>
                     </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    <?php if (count($sales) === 0): ?>
+                        <tr><td colspan="10"><?= htmlspecialchars(__('no_sales_registered')) ?></td></tr>
+                    <?php endif; ?>
+
+                    <?php foreach ($sales as $sale): ?>
+                        <tr>
+                            <td><?= htmlspecialchars((string)$sale['id']) ?></td>
+                            <td><?= htmlspecialchars((string)$sale['sale_date']) ?></td>
+                            <td><?= htmlspecialchars((string)($sale['customer_name'] ?? '-')) ?></td>
+                            <td><?= htmlspecialchars((string)($sale['product_name'] ?? '-')) ?></td>
+                            <td>
+                                <?php if (!empty($sale['batch_id'])): ?>
+                                    <a href="batch_details.php?id=<?= urlencode((string)$sale['batch_id']) ?>">
+                                        <?= htmlspecialchars((string)$sale['batch_id']) ?>
+                                    </a>
+                                <?php else: ?>
+                                    -
+                                <?php endif; ?>
+                            </td>
+                            <td>
+                                <?php if (!empty($sale['harvest_id'])): ?>
+                                    <a href="harvest_details.php?id=<?= urlencode((string)$sale['harvest_id']) ?>">
+                                        <?= htmlspecialchars((string)$sale['harvest_id']) ?>
+                                    </a>
+                                <?php else: ?>
+                                    -
+                                <?php endif; ?>
+                            </td>
+                            <td><?= number_format((float)$sale['quantity'], 2, ',', '.') ?></td>
+                            <td>€ <?= number_format((float)$sale['amount'], 2, ',', '.') ?></td>
+                            <td><?= htmlspecialchars((string)$sale['status']) ?></td>
+                            <td>
+                                <a class="btn" href="traceability_sale_detail.php?id=<?= urlencode((string)$sale['id']) ?>">
+                                    🔎 <?= htmlspecialchars(__('details')) ?>
+                                </a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 
