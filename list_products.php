718b0c99682f8230486f9ed56c1b4e0fd2ca3073
Joep Kuijpers <a.likkle.more@gmail.com>
2026-07-10 15:38:14 +0200

    Add scroll container to products table

--- list_products.php
+++ list_products.php
@@ -1,5 +1,6 @@
 <?php
 include 'includes/header.php';
+include 'includes/language.php';
 include 'includes/sidebar.php';
 include 'db_connect.php';
 
@@ -18,35 +19,37 @@ $products = $db->query("
 <div class="main">
     <h1><?= htmlspecialchars(__('products')) ?></h1>
 
-    <div class="card">
-        <table>
-            <thead>
-                <tr>
-                    <th><?= htmlspecialchars(__('id')) ?></th>
-                    <th><?= htmlspecialchars(__('product')) ?></th>
-                    <th><?= htmlspecialchars(__('category')) ?></th>
-                    <th><?= htmlspecialchars(__('unit')) ?></th>
-                    <th><?= htmlspecialchars(__('sale_price')) ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php if (empty($products)): ?>
+    <div class="card products-table-card">
+        <div class="table-scroll">
+            <table>
+                <thead>
                     <tr>
-                        <td colspan="5"><?= htmlspecialchars(__('no_products_found')) ?></td>
+                        <th><?= htmlspecialchars(__('id')) ?></th>
+                        <th><?= htmlspecialchars(__('product')) ?></th>
+                        <th><?= htmlspecialchars(__('category')) ?></th>
+                        <th><?= htmlspecialchars(__('unit')) ?></th>
+                        <th><?= htmlspecialchars(__('sale_price')) ?></th>
                     </tr>
-                <?php endif; ?>
+                </thead>
+                <tbody>
+                    <?php if (empty($products)): ?>
+                        <tr>
+                            <td colspan="5"><?= htmlspecialchars(__('no_products_found')) ?></td>
+                        </tr>
+                    <?php endif; ?>
 
-                <?php foreach ($products as $product): ?>
-                    <tr>
-                        <td><?= htmlspecialchars((string)$product['id']) ?></td>
-                        <td><?= htmlspecialchars((string)$product['name']) ?></td>
-                        <td><?= htmlspecialchars((string)($product['category'] ?? '-')) ?></td>
-                        <td><?= htmlspecialchars((string)($product['unit'] ?? '-')) ?></td>
-                        <td>€ <?= number_format((float)$product['sale_price'], 2, ',', '.') ?></td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+                    <?php foreach ($products as $product): ?>
+                        <tr>
+                            <td><?= htmlspecialchars((string)$product['id']) ?></td>
+                            <td><?= htmlspecialchars((string)$product['name']) ?></td>
+                            <td><?= htmlspecialchars((string)($product['category'] ?? '-')) ?></td>
+                            <td><?= htmlspecialchars((string)($product['unit'] ?? '-')) ?></td>
+                            <td>€ <?= number_format((float)$product['sale_price'], 2, ',', '.') ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 
