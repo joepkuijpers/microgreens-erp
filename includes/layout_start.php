b806cbc6610e843570a7f418e77a64fbc69cb6ae
joepkuijpers <joepkuijpers@ik.me>
2026-07-27 00:14:51 +0200

    Initialise language helper in shared layouts

--- includes/layout_start.php
+++ includes/layout_start.php
@@ -2,6 +2,8 @@
 $assetPrefix = $assetPrefix ?? '';
 $navPrefix = $navPrefix ?? '';
 
+require_once __DIR__ . '/language.php';
+
 include __DIR__ . '/header.php';
 include __DIR__ . '/sidebar.php';
 ?>
