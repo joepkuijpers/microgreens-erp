db35da32f65628e6e82df9ddbf17f05560f06109
Joep Kuijpers <a.likkle.more@gmail.com>
2026-09-13 00:15:38 +0200

    Refactor: Splits modules B01-B28 in aparte bestanden onder app/modules/

---
+++ app/modules/b24_sales_analytics.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Module: b24_sales_analytics.php
+ * Microgreens ERP - Module Component
+ * 
+ * This file handles the logic and view for the specific module.
+ * It is included by index.php based on the user selection.
+ */
+
+// Beveiliging: Directe toegang voorkomen als nodig (afhankelijk van je architectuur)
+
+// TODO: Implementeer module-specifieke logica hier
+// - Database queries
+// - Form verwerking (POST/GET)
+// - HTML output generatie
+
+echo "<h2>Module $module is geladen</h2>";
+echo "<p>Deze module is nog in ontwikkeling. De structuur is aangemaakt.</p>";
+" ?>
+
